10ce3a0ee775838f714de3d0b7155a363f592cc9
Leonardo Dourado <leonardo.dourado@useblu.com.br>
2024-04-23 14:18:18 -0300

    spec: mock clarity.consent

--- src/providers/setups/clarity/clarity.spec.ts
+++ src/providers/setups/clarity/clarity.spec.ts
@@ -5,13 +5,13 @@ jest.mock('clarity-js', () => ({
   clarity: {
     identify: jest.fn(),
     set: jest.fn(),
+    consent: jest.fn(),
   },
 }));
 
 describe('ClarityProvider', () => {
   it('dispatchUserIdentification should call clarity.identify with correct arguments', () => {
     const userId = 'user123';
-
     ClarityProvider.userIdentification(userId, '');
 
     expect(clarity.identify).toHaveBeenCalledWith(userId);
