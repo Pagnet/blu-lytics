0e421868a82d9c929d74ef059ceaf14575474f0a
Leonardo Dourado <102989712+leonardodouradol@users.noreply.github.com>
2024-04-23 14:46:56 -0300

    chore: update clarity version (#18)

--- src/providers/setups/clarity/clarity.spec.ts
+++ src/providers/setups/clarity/clarity.spec.ts
@@ -5,32 +5,15 @@ jest.mock('clarity-js', () => ({
   clarity: {
     identify: jest.fn(),
     set: jest.fn(),
+    consent: jest.fn(),
   },
 }));
 
 describe('ClarityProvider', () => {
   it('dispatchUserIdentification should call clarity.identify with correct arguments', () => {
     const userId = 'user123';
-
     ClarityProvider.userIdentification(userId, '');
 
     expect(clarity.identify).toHaveBeenCalledWith(userId);
-  });
-
-  it('dispatchCustomEvent should call clarity.set with correct arguments', () => {
-    const eventName = 'clickEvent';
-    const properties = { key: 'value' };
-
-    ClarityProvider.customEvent(eventName, properties);
-
-    expect(clarity.set).toHaveBeenCalledWith(eventName, 'customEvent');
-  });
-
-  it('dispatchScreenEvent should call clarity.set with correct arguments', () => {
-    const screenName = 'HomeScreen';
-
-    ClarityProvider.screenEvent(screenName);
-
-    expect(clarity.set).toHaveBeenCalledWith(screenName, 'screen');
   });
 });
