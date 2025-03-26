ae9d1cb6cf216d507f511018da594db3282c8288
Leonardo Dourado <102989712+leonardodouradol@users.noreply.github.com>
2025-03-26 16:30:13 -0300

    Chore/revert mixpanel reinitialize (#26)

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,20 @@
-/* eslint-disable import/prefer-default-export */
+import mixpanel from 'mixpanel-browser';
 import { ProviderType } from '../providers/provider.types';
 
 export const isValidProvidersList = (
   providersList: ProviderType[],
 ): boolean => Array.isArray(providersList) && providersList.length > 0;
+
+export const checkIfMixPanelIsInitialized = (provider: string): void => {
+  const isMixPanelProvider = provider === 'MixPanel';
+  if (isMixPanelProvider) {
+    const apiKey = localStorage?.getItem('_bl_mp') as string;
+    const wasInitialized = localStorage.getItem('_bl_init');
+
+    if (!wasInitialized) {
+      mixpanel.init(apiKey);
+      localStorage.removeItem('_bl_mp');
+      localStorage.setItem('_bl_init', 'init');
+    }
+  }
+};
